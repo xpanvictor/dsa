83a8a323499449ebbc3621782307709903e77bfc
xpanvictor <xpanvictor@gmail.com>
2023-03-06 18:48:24 +0100

    feat: boilerplate for area calculator

---
+++ oop/AreaCalculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+    private: int radius;
+
+    // todo: include appropriate circle methods
+};
+
+class Utility {
+    // todo: include appropriate class methods
+};
+
+int main(){
+    // first take in circle radius
+    int radius = 0;
+    cout << "Enter the radius of the circle in cm: " << endl;
+    cin >> radius;
+
+    // todo: princess code here
+    return 0;
+}
