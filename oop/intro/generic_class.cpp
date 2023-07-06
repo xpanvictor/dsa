64f316635886f03207acb68f4e297a1a6956f382
xpan <xpanvictor@gmail.com>
2023-07-06 17:57:35 +0100

    feat: Personal class header declaration

--- oop/intro/generic_class.cpp
+++ oop/intro/generic_class.cpp
@@ -1,19 +1,28 @@
+#include <iostream>
+using namespace std;
 
 template<class genType, int size=50>
 class genClass{
   genType storage[size];
-}
+};
 
 // Initializing the types
 genClass<int> intObject;
 genClass<float> floatObject;
-genclass<int, 100> intObject2;
+genClass<int, 100> intObject2;
 
 // using the template structure for swap fn 
 template<class genMeth>
-void swap(genMet& el1, genMeth& el2) {
+void swap(genMeth& el1, genMeth& el2) {
   genMeth tmp = el1;
   el1 = el2;
   el2 = tmp;
+}
+
+int main () {
+  genMeth<int> el1 = 5, el2 = 7;
+  swap(el1, el2);
+  cout << el1 << endl;
+  return 0;
 }
 
--- oop/intro/pointers_obj.cpp
+++ oop/intro/pointers_obj.cpp
@@ -39,6 +39,35 @@ struct Node {
   }
 };
 
+double root(double (*fn)(double), double a, double b, double epsilon) {
+    double midPoint = (a + b) / 2;
+    // loop if result isnt root already or range of interval isnt below epsilon
+    while(fn(midPoint) != 0 or fabs(b-a) > epsilon) {
+      // check if left limit and midPoint have opp signs
+      if(fn(midPoint) * fn(a) < 0) // hence, continue in new left interval
+        b = midPoint;
+      else a = midPoint;
+      midPoint = (a + b) / 2;
+    }
+
+    return midPoint; // finally, we have apxm root
+  }
+
+ double f(double x) {
+  return x * 2;
+}
+
+// A summation function to find doubles using pointer of the function
+double sum(double (*f)(double), int l_lmt, int u_lmt) {
+  double result = 0;
+    for (int i = l_lmt, i < u_lmt, i++) {
+      result += f(i);
+    }
+
+    return result;
+}
+
+
 int main() {
   Node node1 ("victor", 5), node2(node1);
   Node node3 = node2;
@@ -53,37 +82,10 @@ int main() {
   // WARNING: use Reference Variables carefully, it destroys the information hiding principle
   //
   // ----------Pointers to functions
-  double f(double x) {
-    return x * 2;
-  }
-
-  // A summation function to find doubles using pointer of the function
-  double sum(double (*f)(double), int l_lmt, int u_lmt) {
-    double result = 0;
-    for (int i = l_lmt, i < u_lmt, i++) {
-      result += f(i);
-    }
-
-    return result;
-  }
   cout << sum(f, 1, 5) << endl;
 
   // Use in root of a continuous fn in an interval, a-b
-  double root(double (*fn)(double), double a, double b, double epsilon) {
-    double midPoint = (a + b) / 2;
-    // loop if result isnt root already or range of interval isnt below epsilon
-    while(fn(midPoint) != 0 or fabs(b-a) > epsilon) {
-      // check if left limit and midPoint have opp signs
-      if(fn(midPoint) * fn(a) < 0) // hence, continue in new left interval
-        b = midPoint;
-      else a = midPoint;
-      midPoint = (a + b) / 2;
-    }
-
-    return midPoint; // finally, we have apxm root
-  }
-
-  return 0;
+    return 0;
 }
 
 
