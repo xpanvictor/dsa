b24aebdba4a0e22ac8872aafce2bd8e5db8275c5
xpanvictor <xpanvictor@gmail.com>
2024-03-26 20:20:25 +0100

    feat: to continue with excessive recursion

---
+++ recursion/README.md
@@ -0,0 +1,4 @@
+# Algorithms in Basic
+1. Reversing characters with non tail recursive functions.
+2. The nested sum function of h(n) = h(2 + h(n*2))
+3. The Ackermann and Rozsa function to show growth with recursion
--- recursion/basics.cpp
+++ recursion/basics.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// Reversing characters
 void
 reverse_chars ()
 {
@@ -15,9 +16,41 @@ reverse_chars ()
     }
 }
 
+// Nested recursion
+int
+nested_sum (int n)
+{
+  // cout << "case: " << n << endl;
+  if (n == 0 || n > 4)
+    return n;
+  return nested_sum (2 + nested_sum (n * 2));
+}
+
+// Ackerman & Rozsa function
+int
+A (int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  if (n > 0 && m == 0)
+    return A (n - 1, 1);
+  return A (n - 1, A (n, m - 1));
+}
+
 int
 main ()
 {
   cout << "Testing reverse_chars with nontail recursion; type in chars: ";
   reverse_chars ();
+  cout << endl;
+
+  cout << "Testing nested sum of h(n) = h(2 + h(n * 2)) if n <= 4 != 0"
+       << endl;
+  cout << nested_sum (2) << endl;
+
+  cout
+      << "Testing Ackermann and Rozsa function with A(3,1) and A(4,1) to show "
+         "growth:"
+      << endl;
+  cout << A (3, 1) << " grows to " << A (4, 1) << endl;
 }
