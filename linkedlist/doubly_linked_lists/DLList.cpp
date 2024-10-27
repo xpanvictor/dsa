559263622f6d2a94a47aa339772dbe0311f0961c
xpanvictor <xpanvictor@gmail.com>
2024-10-27 22:28:00 +0100

    feat(recursion): corrects implementations

--- linkedlist/doubly_linked_lists/DLList.cpp
+++ linkedlist/doubly_linked_lists/DLList.cpp
@@ -2,31 +2,33 @@
 #include "DLList.h"
 
 template <class T>
-void DoublyLinkedList<T>::addToDLLTail(const T &el)
+void
+DoublyLinkedList<T>::addToDLLTail (const T &el)
 {
-    if (tail != 0)
+  if (tail != 0)
     {
-        tail = new DLLNode(el, 0, tail);
-        tail->prev->next = tail;
+      tail = new DLLNode (el, 0, tail);
+      tail->prev->next = tail;
     }
-    else
-        head = tail = new DLLNode(el);
+  else
+    head = tail = new DLLNode (el);
 }
 
 template <class T>
-T DoublyLinkedList<T>::deleteFromDLLTail()
+T
+DoublyLinkedList<T>::deleteFromDLLTail ()
 {
-    T el = tail->info;
-    if (tail == head)
+  T el = tail->info;
+  if (tail == head)
     {
-        delete head;
-        tail = head = 0;
+      delete head;
+      tail = head = 0;
     }
-    else
+  else
     {
-        tail = tail->prev;
-        delete tail->next;
-        tail->next = 0;
+      tail = tail->prev;
+      delete tail->next;
+      tail->next = 0;
     }
-    return el;
+  return el;
 }
--- linkedlist/doubly_linked_lists/DLList.h
+++ linkedlist/doubly_linked_lists/DLList.h
@@ -1,41 +1,37 @@
 #ifndef DOUBLY_LINKED_LIST
 #define DOUBLY_LINKED_LIST
 
-template <class T>
-class DLLNode
+template <class T> class DLLNode
 {
 public:
-    DLLNode()
-    {
-        prev = next = 0;
-    }
+  DLLNode () { prev = next = 0; }
 
-    DLLNode(const T &el, DLLNode *pv = 0, DLLNode *nx = 0)
-    {
-        info = el;
-        prev = pv, next = nx;
-    }
+  DLLNode (const T &el, DLLNode *pv = 0, DLLNode *nx = 0)
+  {
+    info = el;
+    prev = pv, next = nx;
+  }
 
-    DLLNode *prev, *next;
-    T info;
+  DLLNode *prev, *next;
+  T info;
 };
 
-template <class T>
-class DoublyLinkedList
+template <class T> class DoublyLinkedList
 {
 public:
-    DOUBLY_LINKED_LIST()
-    {
-        head = tail = 0;
-    }
+  void
+  DOUBLY_LINKED_LIST ()
+  {
+    head = tail = 0;
+  }
 
-    ~DOUBLY_LINKED_LIST();
+  ~DOUBLY_LINKED_LIST ();
 
-    void addToDLLTail(const T &el);
-    T deleteFromDLLTail();
+  void addToDLLTail (const T &el);
+  T deleteFromDLLTail ();
 
 private:
-    DLLNode<T> *head, *tail;
+  DLLNode<T> *head, *tail;
 };
 
 #endif // DOUBLY_LINKED_LIST
